ca0ba224a7daa093eceb734f157c4b3a5ddb25a2
Rachel Mailach <rachel.mailach@gmail.com>
2016-02-20 00:57:28 -0500

    Modifying to PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Rachel Mailach</title>
+		<!-- link to main stylesheet -->
+		<link rel="stylesheet" type="text/css" href="/css/main.css">
+	</head>
+	<body>
+        <?php include("/includes/header.html");?>
+
+		
+        <div class="page_content">
+    		<div class="blurb">
+        		<h1>Rachel Mailach</h1>
+				<p>I'm best known as the one who brings cookies to work. Don't trust me. <br><a href="about.html">Read more about me...</a></p>
+    		</div><!-- /.blurb -->
+		</div>
+
+        
+        <?php include("/includes/footer.html");?>
+	</body>
+</html>
